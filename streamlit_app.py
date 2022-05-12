5c7d05618d0ea569ba81d2cf199c2a849cd8aa5a
João Cassis <joaopcassis@gmail.com>
2022-05-12 22:16:25 +0100

    Retrying statcounter with components

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,22 @@ import pydeck as pdk
 import streamlit as st
 import pandas as pd
 import utils
+import streamlit.components.v1 as components
+
+components.html('''<!-- Default Statcounter code for Casas
+https://share.streamlit.io/kassiusklay/casas/main -->
+<script type="text/javascript">
+var sc_project=12752694;
+var sc_invisible=1;
+var sc_security="91e69369";
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12752694/0/91e69369/1/" alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->''')
 
 st.set_page_config(layout='wide')
 engine = utils.init_engine()
