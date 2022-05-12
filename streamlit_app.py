687c46b684bcea9f99076f86874deef5d8b3d8ea
João Cassis <joaopcassis@gmail.com>
2022-05-12 22:24:09 +0100

    statcounter component did not work

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,8 @@ import pydeck as pdk
 import streamlit as st
 import pandas as pd
 import utils
-import streamlit.components.v1 as components
 
-# st.set_page_config(layout='wide')
-
-components.html("""
-    <script type="text/javascript">
-    var sc_project=12752694;
-    var sc_invisible=1;
-    var sc_security="91e69369";
-    </script>
-    <script type="text/javascript"
-    src="https://www.statcounter.com/counter/counter.js" async></script>
-    <noscript><div class="statcounter"><a title="Web Analytics"
-    href="https://statcounter.com/" target="_blank"><img class="statcounter"
-    src="https://c.statcounter.com/12752694/0/91e69369/1/" alt="Web Analytics"
-    referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-    """)
-
+st.set_page_config(layout='wide')
 engine = utils.init_engine()
 
 
