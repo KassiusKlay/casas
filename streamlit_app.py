7bb8bf1881b8b5831dacc41ec5f30ecff3378e5c
João Cassis <joaopcassis@gmail.com>
2022-05-19 21:30:35 +0100

    removed script to analytics (now there is default in streamlit) and changed gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -427,24 +427,6 @@ def main():
         show_map(map_df, color_selection)
         show_listing(map_df)
 
-    components.html("""<!-- Default Statcounter code for Casas
-    https://share.streamlit.io/kassiusklay/casas/main -->
-    <script type="text/javascript">
-    var sc_project=12752694;
-    var sc_invisible=1;
-    var sc_security="91e69369";
-    </script>
-    <script type="text/javascript"
-    src="https://www.statcounter.com/counter/counter.js"
-    async></script>
-    <noscript><div class="statcounter"><a title="Web Analytics"
-    href="https://statcounter.com/" target="_blank"><img
-    class="statcounter"
-    src="https://c.statcounter.com/12752694/0/91e69369/1/"
-    alt="Web Analytics"
-    referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-    <!-- End of Statcounter Code -->""")
-
 
 if __name__ == '__main__':
     main()
